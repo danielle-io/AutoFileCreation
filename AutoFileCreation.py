60ef3a642dc26c5952938c3fdd8b335514b8f665
Danielle Muhlenberg <danieyells@gmail.com>
2024-07-07 16:01:32 -0700

    Adding new model option

--- AutoFileCreation.py
+++ AutoFileCreation.py
@@ -23,18 +23,36 @@ example_item_name_camel_case = example_item_name[0].lower() + example_item_name[
 def main():
     # ------ Start: Put each set of file names and locations here
 
-    # IStorage
+    # IStorages
     i_storage_file_name = 'IStorageBroker._s.cs'
     location = f'{destination}{app_name}.Api\\Brokers\\Storages\\'      
     copy_example_to_new_file(i_storage_file_name, location)
 
-    # Storage
+    # Storages
     storage_file_name = 'StorageBroker._s.cs'
     location = f'{destination}{app_name}.Api\\Brokers\\Storages\\'
     copy_example_to_new_file(storage_file_name, location)
-
+    
+    # Controllers
+    controller_file_name = '_sController.cs'
+    location = f'{destination}{app_name}.Api\\Controllers\\'
+    copy_example_to_new_file(controller_file_name, location)
 
 # ------ End: Put each set of file names and locations here
+
+def create_basic_model(fill_in_file_name, file_path):
+    # Add to empty file
+    new_file_name = fill_in_file_name.replace('_', new_value)
+    
+    if (is_mac == 'true'):
+        file_path.replace('\\', '/')
+    
+    example_item_file_name = fill_in_file_name.replace('_', example_item_name)
+    new_file_path = f'{file_path}{new_file_name}'
+    file_content = 'This is the content of the new file.'
+
+    with open(new_file_path, 'w') as file:
+        file.write(content)
 
 def copy_example_to_new_file(fill_in_file_name, file_path):
     # Copy to empty file
