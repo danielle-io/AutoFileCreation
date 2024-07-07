8f5da9961b8e4d8d822e1e339d03b63034308d03
Danielle Muhlenberg <danieyells@gmail.com>
2024-07-07 15:15:08 -0700

    Switched to .env

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# AutoFileCreation
+Create a .env with:
+
+EXAMPLE_ITEM_NAME=<var goes here>
+New_VALUE=<var goes here>
+APP=<var goes here>
+DESTINATION=<var goes here>
--- main.py
+++ main.py
@@ -1,13 +1,18 @@
+import shutil
 import os
+from dotenv import load_dotenv
 
-# ---- Start: Variable Changes ----
-app_name = 'appName.Core'
-destination_path = 'C:/Users/pathGoesHere'
+# Load environment variables from .env file
+load_dotenv()
+    
+# Retrieve environment variables
+example_item_name = os.getenv('EXAMPLE_ITEM_NAME')
+new_value = os.getenv('NEW_VALUE')
+app_name = os.getenv('APP_NAME')
+destination = os.getenv('DESTINATION')
 
-# Keep singular and in titlecase
-new_model_item_name = 'CareAnimal'
-example_item_name = 'CarePerson'
-# ---- End Variable Changes ----
+if not example_item_name or not new_value or not app or not destination:
+    raise ValueError("Environment variables 'EXAMPLE_ITEM_NAME' and 'NEW_VALUE' must be set")
 
 new_model_item_name_camel_case = new_model_item_name[0].lower() + new_model_item_name[1:]
 example_item_name_camel_case = example_item_name[0].lower() + example_item_name[1:]
@@ -16,23 +21,23 @@ def main():
 	# ------ Start: Put each set of file names and locations here
 
 	# IStorage
-	i_storage_file_name = IStorageBroker._s.cs
+	i_storage_file_name = 'IStorageBroker._s.cs'
 	location =  f'{destination_path}{app_name}.Api/Brokers/Storages/'
 	copy_example_to_new_file(i_storage_file_name, location)
 
 	# Storage
-	storage_file_name = StorageBroker._s.cs
+	storage_file_name = 'StorageBroker._s.cs'
 	location =  f'{destination_path}{app_name}.Api/Brokers/Storages/'
 	copy_example_to_new_file(storage_file_name, location)
 
 	# ------ End: Put each set of file names and locations here
 
-def copy_example_to_new_file(storage_file_name, file_path, new_model_item_name):
+def copy_example_to_new_file(storage_file_name, file_path):
 	# Copy to empty file
 	file_name = storage_file_name.replace('_', new_model_item_name)
 	example_item_file_name = file_name.replace('_', example_item_name)
-	new_file_path = f'{location}{file_name}'
-	example_file.copyfile(f'{location}{example_item_file_name}', new_file_path)
+	new_file_path = f'{file_path}{file_name}'
+	shutil.copyfile(f'{file_path}{example_item_file_name}', new_file_path)
 	
 	# Replace content
 	with open(file_path, 'r') as file: content = file.read()
